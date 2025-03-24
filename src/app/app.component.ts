4517a60fcf1657464557a7249aaffe2b230c42db
simon-kercode <sim.malry@gmail.com>
2025-03-24 10:45:01 +0100

    quiz config form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { ConfigComponent } from "./components/config/config.component";
 
 @Component({
     selector: "app-root",
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, ConfigComponent],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.css",
 })
