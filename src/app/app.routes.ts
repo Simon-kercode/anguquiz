cbe759067ab13f6dbda09bd89f6e890f10f86c9b
simon-kercode <sim.malry@gmail.com>
2025-03-24 12:03:13 +0100

    profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from "@angular/router";
+import { ConfigComponent } from "./components/config/config.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'config', component: ConfigComponent },
+
+    { path: '', redirectTo: '/config', pathMatch: 'full' },
+  ];
