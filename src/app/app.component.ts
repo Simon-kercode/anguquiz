4093fa542932e93ebe7e270b6ba7b6f7abf3210b
simon-kercode <sim.malry@gmail.com>
2025-03-24 13:50:51 +0100

    warning fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { ConfigComponent } from "./components/config/config.component";
 
 @Component({
     selector: "app-root",
-    imports: [RouterOutlet, ConfigComponent],
+    imports: [RouterOutlet],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.css",
 })
