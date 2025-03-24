1280208f667e4b4b44b64cc16f9b3cd4f6f1942e
simon-kercode <sim.malry@gmail.com>
2025-03-24 14:50:01 +0100

    questions display

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrentQuizService } from '../../service/current-quiz.service';
+import { KnownQuestion } from '../../model/question';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  selectedAnswer: string | null = null;
 
+  constructor(public quizService: CurrentQuizService) {}
+
+  ngOnInit(): void {
+    if (!this.quizService.questions()) {
+      console.error("quiz non initialisé !");
+    }
+  }
+
+  onSelectAnswer(answer: string) {
+    this.selectedAnswer = answer;
+  }
+
+  onSubmit() {
+    if (this.selectedAnswer === this.quizService.currentQuestion()?.correct_answer) {
+      this.quizService.incrementCorrectAnswers();
+    }
+    this.selectedAnswer = null;
+    this.quizService.next();
+  }
 }
