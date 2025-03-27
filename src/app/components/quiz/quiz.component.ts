8cd3dad5c9f3a0e734d803db2d34d6ee5babc639
Loxoz <37414842+Loxoz@users.noreply.github.com>
2025-03-27 14:55:51 +0100

    reformat quiz.component.ts

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,46 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CurrentQuizService } from '../../service/current-quiz.service';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CurrentQuizService } from "../../service/current-quiz.service";
+import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-quiz',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+    selector: "app-quiz",
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: "./quiz.component.html",
+    styleUrl: "./quiz.component.css",
 })
 export class QuizComponent implements OnInit {
-  selectedAnswer: string | null = null;
-  isAnswered: boolean = false;
+    selectedAnswer: string | null = null;
+    isAnswered: boolean = false;
 
-  constructor(
-    public quizService: CurrentQuizService,
-    private router: Router,
-  ) {}
+    constructor(
+        public quizService: CurrentQuizService,
+        private router: Router,
+    ) {}
 
-  ngOnInit(): void {
-    if (!this.quizService.questions()) {
-      console.error("quiz non initialisé !");
+    ngOnInit(): void {
+        if (!this.quizService.questions()) {
+            console.error("quiz non initialisé !");
+        }
     }
-  }
 
-  onSubmit() {
-    if (!this.selectedAnswer) return;
-    this.isAnswered = true;
-    if (this.selectedAnswer === this.quizService.currentQuestion()?.correct_answer) {
-      this.quizService.incrementCorrectAnswers();
+    onSubmit() {
+        if (!this.selectedAnswer) return;
+        this.isAnswered = true;
+        if (this.selectedAnswer === this.quizService.currentQuestion()?.correct_answer) {
+            this.quizService.incrementCorrectAnswers();
+        }
     }
-  }
 
-  nextQuestion() {
-    this.selectedAnswer = null;
-    this.isAnswered = false;
-    this.quizService.next();
+    nextQuestion() {
+        this.selectedAnswer = null;
+        this.isAnswered = false;
+        this.quizService.next();
 
-    if (this.quizService.finished()) {
-      this.router.navigate(["results"]);
+        if (this.quizService.finished()) {
+            this.router.navigate(["results"]);
+        }
     }
-  }
 }
